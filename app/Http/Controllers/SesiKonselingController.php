8a0ecaa3bb260669211ec3ea2697382443578e19
Gesyarhn <gesyareihan1812@gmail.com>
2025-04-12 22:41:12 +0700

    Membuat view, model sesi konseling, dan controller index serta store

--- app/Http/Controllers/SesiKonselingController.php
+++ app/Http/Controllers/SesiKonselingController.php
@@ -10,5 +10,27 @@ use Illuminate\Support\Facades\Storage;
 
 class SesiKonselingController extends Controller
 {
+    public function index()
+    {
+        $sesi_konseling = SesiKonseling::get();
+        $konselor = Konselor::get();
 
+        return view('admin.sesi_konseling.index',compact('sesi_konseling','konselor'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+
+            'konselor_id' => 'required',
+            'sesi' => 'required',
+            'hari' => 'required',
+            'status' => 'required',
+            
+        ]);
+
+        SesiKonseling::create($validatedData);
+
+        return response()->json(['message' => 'Data created successfully']);
+    }
 }
