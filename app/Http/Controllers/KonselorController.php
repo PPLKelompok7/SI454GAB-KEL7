20238fbe853c5d7d83600a6ec422c210b7c6e327
jessicainas27 <jessicarasendriya577@gmail.com>
2025-04-17 14:23:41 +0700

    Update KonselorController.php
    Menambahkan fungsi create, edit

--- app/Http/Controllers/KonselorController.php
+++ app/Http/Controllers/KonselorController.php
@@ -9,5 +9,47 @@ use Illuminate\Support\Facades\Storage;
 
 class KonselorController extends Controller
 {
-    
+    public function index()
+    {
+        $konselor = Konselor::get();
+        $user = User::where('is_role',"Konselor")->get();
+
+        return view('admin.konselor.index',compact('konselor','user'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+
+            'user_id' => 'required',
+            'nip' => 'required',
+            'no_telepon' => 'required',
+            'deskripsi' => 'required',
+            'gambar' => 'required|image',
+            
+        ]);
+
+        if ($request->deskripsi) {
+            $validatedData['deskripsi'] = nl2br($request->deskripsi);
+        }
+
+        if ($request->hasFile('gambar')) {
+            $validatedData['gambar'] = $request->file('gambar')->store('img-foto-konselor');
+        }
+
+        Konselor::create($validatedData);
+
+        return response()->json(['message' => 'Data created successfully']);
+    }
+    public function show($id)
+    {
+        $data = Konselor::with('user')->find($id);
+        return response()->json($data);
+    }
+    public function edit($id)
+    {
+        $data = Konselor::find($id);
+        return response()->json($data);
+    }
+
 }
