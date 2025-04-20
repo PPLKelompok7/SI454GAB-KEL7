3da4876d43a1bd5d81bcd593ae1bcca219cd83af
Putrarahimip <91724920+Putrarahimip@users.noreply.github.com>
2025-04-20 16:38:24 +0700

    push pendaftaran konseling (1)

---
+++ resources/views/mahasiswa/pendaftaran_konseling.blade.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PendaftaranKonseling;
+use App\Models\SesiKonseling;
+use Illuminate\Http\Request;
+
+class PendaftaranKonselingController extends Controller
+{
+    public function index()
+    {
+        $data = PendaftaranKonseling::get();
+        return view('admin.pendaftaran_konseling.index',compact('data'));       
+    }
+
+    public function show($id)
+    {
+        $data = PendaftaranKonseling::with('sesiKonseling.konselor.user','mahasiswa')->find($id);
+        // dd($data);
+        return response()->json($data);
+    }
+    public function edit($id)
+    {
+        $data = PendaftaranKonseling::with('sesiKonseling.konselor.user','mahasiswa')->find($id);
+        return response()->json($data);
+    }
+    public function update($id, Request $request)
+    {
+        $sesi_konseling = PendaftaranKonseling::findOrFail($id); 
+
+        $validatedData = $request->validate([           
+            'status' => 'required',
+            'link' => 'required',
+        ]);
+
+        $sesi_konseling->update($validatedData);
+
+        if ($validatedData['status'] == 'Selesai') {
+            SesiKonseling::where('id', $sesi_konseling->sesi_konseling_id)
+                ->update(['status' => 'Tersedia']);
+        }
+
+        return response()->json(['message' => 'Data Updated successfully']);
+    }
+
+    
+    // public function destroy($id)
+    // {
+    //     $sesi_konseling = PendaftaranKonseling::find($id);
+    //     $sesi_konseling->delete();
+    //     return response()->json(['message' => 'Data deleted successfully']);
+    // }
+}
