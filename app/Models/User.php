fc85160f8149e50c7281804196b6d6c547dbad12
Gesyarhn <107228216+Gesyarhn@users.noreply.github.com>
2025-04-28 14:37:30 +0700

    Revert "Features kelola sesi jadwal konseling"

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,47 +11,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'password',
-    // ];
-    protected $guarded = ['id'];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function konselor()
-    {
-        return $this->hasOne(Konselor::class);
-    }
-
-    public function pendaftaranKonseling()
-    {
-        return $this->hasMany(PendaftaranKonseling::class, 'mahasiswa_id');
-    }
 }
 
