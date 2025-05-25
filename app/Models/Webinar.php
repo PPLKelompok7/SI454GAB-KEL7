bee5ec7b7e15ab6a7798bcaf0fceae3f5b8e271d
Putrarahimip <Putrarahimip@student.telkomuniversity.ac.id>
2025-05-25 22:28:46 +0700

    fixing models pada fitur webinar

--- app/Models/Webinar.php
+++ app/Models/Webinar.php
@@ -11,7 +11,7 @@ class Webinar extends Model
 
     protected $guarded = ['id'];
 
-    public function use()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
